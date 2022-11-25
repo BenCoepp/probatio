237bf918afe15f95aadcdefe9c3ea0cf3910a2c4
Ben Coepp <bencoepp@gmail.com>
2022-11-25 16:17:27 +0100

    update

--- src/main/java/de/bencoepp/utils/CommandHelper.java
+++ src/main/java/de/bencoepp/utils/CommandHelper.java
@@ -4,44 +4,58 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
 
 public class CommandHelper {
 
     public static boolean executeCommand(String[] commands, File dir) throws IOException, IOException {
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(commands,null, dir);
-
-        BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(proc.getInputStream()));
-
-        BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(proc.getErrorStream()));
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-        }
         boolean ok = true;
-        while ((s = stdError.readLine()) != null) {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(commands,null, dir);
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(proc.getErrorStream()));
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+            }
+
+            while ((s = stdError.readLine()) != null) {
+                ok = false;
+            }
+
+        }catch (Exception e){
             ok = false;
+            System.out.println(e);
         }
         return ok;
     }
 
     public static String executeCommandWithOutput(String[] commands) throws IOException, IOException {
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(commands);
-
-        BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(proc.getInputStream()));
-
-        BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(proc.getErrorStream()));
         String output = "";
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-            output += s;
-        }
-        while ((s = stdError.readLine()) != null) {
-            output += s;
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(commands);
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(proc.getErrorStream()));
+
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                output += s;
+            }
+            while ((s = stdError.readLine()) != null) {
+                output += s;
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
         }
         return output;
     }
